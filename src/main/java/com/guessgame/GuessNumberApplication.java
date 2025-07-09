1ae830be07cd015473b20d1d8ed3e0f60988f4b8
Hieu Tran <tranvanphuochieu@gmail.com>
2025-07-09 12:00:14 +0700

    Initial commit

---
+++ src/main/java/com/guessgame/GuessNumberApplication.java
@@ -0,0 +1,13 @@
+package com.guessgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessNumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuessNumberApplication.class, args);
+    }
+
+}
