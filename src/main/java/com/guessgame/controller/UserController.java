dca1a1ad218f017f68e232e93196e418b4634c58
Hieu Tran <tranvanphuochieu@gmail.com>
2025-07-11 16:25:42 +0700

    Rename info of api /me

--- src/main/java/com/guessgame/controller/UserController.java
+++ src/main/java/com/guessgame/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
         return Map.of(
                 "email", user.getEmail(),
                 "score", user.getScore(),
-                "turns", user.getTurns()
+                "turnsLeft", user.getTurns()
         );
     }
 }
